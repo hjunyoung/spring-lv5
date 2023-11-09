5b57126496414a3f5ac0414e9a527a103d58cd04
Joonyoung Hong <hjunyoung10@gmail.com>
2023-11-09 20:27:02 +0900

    Project setup

---
+++ src/main/java/com/example/springlv5/SpringLv5Application.java
@@ -0,0 +1,13 @@
+package com.example.springlv5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLv5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLv5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springlv5/SpringLv5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springlv5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLv5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
